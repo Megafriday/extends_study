c7693793f3ec8c4452aa9cf98c0e8db48383f9c7
Megafriday <megafriday@yahoo.co.jp>
2020-10-27 23:04:15 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,20 @@
 class Main {
 	public static void main(String[] args) {
 		Parent parent = new Child();
-		System.out.println(parent.str); 	// これは、Parentです。
+		System.out.println(parent.str); 	       // これは、Parentです。
 		parent.sameMethod();				// これは、Childです。
-		//		parent.onlyChild();					// コンパイルエラー
-		parent.onlyParent();				//onlyParent()メソッドです。
+		// parent.onlyChild();		    		   // コンパイルエラー
+		parent.onlyParent();				   // onlyParent()メソッドです。
 
 
 		System.out.println("--------------------------------------");
 
 
 		Child child = new Child();
-		System.out.println(child.str);		// これは、Childです。
-		child.sameMethod(); 				// これは、Childです。
-		child.onlyChild(); 					// onlyChild()メソッドです。
-		child.onlyParent(); 				// onlyParent()メソッドです。
+		System.out.println(child.str);			// これは、Childです。
+		child.sameMethod();				 // これは、Childです。
+		child.onlyChild(); 				     // onlyChild()メソッドです。
+		child.onlyParent(); 				    // onlyParent()メソッドです。
 
 	}
 }
